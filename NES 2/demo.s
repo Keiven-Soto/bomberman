b1021b431e3e5246d6d71d32acebdc485699a9f1
Keiven-Soto <78508728+Keiven-Soto@users.noreply.github.com>
2024-03-30 22:38:25 -0400

    sprites should appear

--- NES 2/demo.s
+++ NES 2/demo.s
@@ -1,5 +1,4 @@
-; Keiven Soto
-; 802-19-3707
+;Noel Vargas Padilla 802-19-7297
 
 .segment "HEADER"
   ; .byte "NES", $1A      ; iNES header identifier
@@ -22,6 +21,8 @@
 ; Main code segment for the program
 .segment "CODE"
 
+.include "constants.inc"
+
 reset:
   sei		; disable IRQs
   cld		; disable decimal mode
@@ -30,13 +31,13 @@ reset:
   ldx #$ff 	; Set up stack
   txs		;  .
   inx		; now X = 0
-  stx $2000	; disable NMI
-  stx $2001 	; disable rendering
+  stx PPUCTRL	; disable NMI
+  stx PPUMASK 	; disable rendering
   stx $4010 	; disable DMC IRQs
 
 ;; first wait for vblank to make sure PPU is ready
 vblankwait1:
-  bit $2002
+  bit PPUSTATUS
   bpl vblankwait1
 
 clear_memory:
@@ -51,84 +52,100 @@ clear_memory:
   sta $0700, x
   inx
   bne clear_memory
-
+  
 ;; second wait for vblank, PPU is ready after this
 vblankwait2:
-  bit $2002
+  bit PPUSTATUS
   bpl vblankwait2
 
+.export main
 main:
-load_palettes:
-  lda $2002 ;reads from the CPU-RAM PPU address register to reset it
-  lda #$3f  ;loads the higher byte of the PPU address register of the palettes in a (we want to write in $3f00 of the PPU since it is the address where the palettes of the PPU are stored)
-  sta $2006 ;store what's in a (higher byte of PPU palettes address register $3f00) in the CPU-RAM memory location that transfers it into the PPU ($2006)
-  lda #$00  ;loads the lower byte of the PPU address register in a
-  sta $2006 ;store what's in a (lower byte of PPU palettes address register $3f00) in the CPU-RAM memory location that transfers it into the PPU ($2006)
-  ldx #$00  ;AFTER THIS, THE PPU-RAM GRAPHICS POINTER WILL BE POINTING TO THE MEMORY LOCATION THAT CONTAINS THE SPRITES, NOW WE NEED TO TRANSFER SPRITES FROM THE CPU-ROM TO THE PPU-RAM
-            ;THE PPU-RAM POINTER GETS INCREASED AUTOMATICALLY WHENEVER WE WRITE ON IT
+  load_sprites:
+    lda PPUSTATUS
+    lda #$00
+    sta OAMADDR
 
-; NO NEED TO MODIFY THIS LOOP SUBROUTINE, IT ALWAYS LOADS THE SAME AMOUNT OF PALETTE REGISTER. TO MODIFY PALETTES, REFER TO THE PALETTE SECTION
-@loop: 
-  lda palettes, x   ; as x starts at zero, it starts loading in a the first element in the palettes code section ($0f). This address mode allows us to copy elements from a tag with .data directives and the index in x
-  sta $2007         ;THE PPU-RAM POINTER GETS INCREASED AUTOMATICALLY WHENEVER WE WRITE ON IT
-  inx
-  cpx #$4C
-  bne @loop
+    ldx #0
+    loop_load_sprites:
+      lda sprites, X
+      sta OAMDATA
+      inx
+      cpx #64
+      bne loop_load_sprites
 
-enable_rendering: ; DO NOT MODIFY THIS
+  load_palettes:
+    lda PPUSTATUS
+    lda #$3f
+    sta PPUADDR
+    lda #$00
+    sta PPUADDR
+
+    ldx #$00
+    @loop:
+      lda palettes, x
+      sta PPUDATA
+      inx
+      cpx #$20
+      bne @loop
+
+enable_rendering:
   lda #%10000000	; Enable NMI
-  sta $2000
-  lda #%00010000	; Enable Sprites
-  sta $2001
+  sta PPUCTRL
+  lda #%00010110; Enable background and sprite rendering in PPUMASK.
+  sta PPUMASK
 
-forever: ;FOREVER LOOP WAITING FOR THEN NMI INTERRUPT, WHICH OCCURS WHENEVER THE LAST PIXEL IN THE BOTTOM RIGHT CORNER IS PROJECTED
+forever:
   jmp forever
 
-nmi:  ;WHENEVER AN NMI INTERRUPT OCCURS, THE PROGRAM JUMPS HERE (60fps)
-  ldx #$00 	; Set SPR-RAM address to 0
-  stx $2003 ;Sets the PPU-RAM pointer to $2003 to start receiving sprite information saved under the tag "hello"
-@loop:	lda hello, x 	; Load the hello message into SPR-RAM one by one, the pointer is increased every time a byte is written. Sprites are referenced by using the third byte of the 4-byte arrays in "hello"
-  sta $2004
-  inx
-  cpx #$4c            ;ATTENTION: if you add more letters, you must increase this number by 4 per each additional letter. This is the limit for the sprite memory copy routine
-  bne @loop
+nmi:
+  lda #$00
+  sta PPUSCROLL
+  lda #$00
+  sta PPUSCROLL
+
   rti
 
-hello:
-  .byte $00, $00, $00, $00 	; DO NOT MODIFY THESE
-  .byte $00, $00, $00, $00  ; DO NOT MODIFY THESE
-  .byte $6c, $00, $00, $28  ; K
-  .byte $6c, $01, $01, $32  ; E
-  .byte $6c, $02, $02, $3A  ; I
-  .byte $6c, $03, $03, $42  ; V
-  .byte $6c, $01, $00, $4B  ; E
-  .byte $6c, $05, $01, $56  ; N
 
-  .byte $80, $06, $02, $6c  ; S
-  .byte $80, $07, $03, $76  ; O
-  .byte $80, $08, $00, $80  ; T
-  .byte $80, $07, $01, $8A  ; O
+palettes:
+; background palette
+.byte $0F, $16, $13, $37
+.byte $00, $00, $00, $00
+.byte $00, $00, $00, $00
+.byte $00, $00, $00, $00
 
-  .byte $94, $0A, $00, $9E  ; C
-  .byte $94, $0B, $01, $A8  ; A
-  .byte $94, $0C, $02, $B2  ; B
-  .byte $94, $0D, $03, $BC  ; R
-  .byte $94, $0E, $03, $C6  ; E
-  .byte $94, $0D, $01, $D0  ; R
-  .byte $94, $0B, $02, $DA  ; A
+; sprite palette
+.byte $0F, $16, $13, $37
+.byte $00, $00, $00, $00
+.byte $00, $00, $00, $00
+.byte $00, $00, $00, $00
 
-palettes: ;The first color should always be the same accross all the palettes. MOdify this section to determine which colors you'd like to use
-  ; Background Palette % all black and gray
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
-  .byte $0f, $00, $00, $00
 
-  ; Sprite Palette  %notice that the first palette contains the white color in the second element
-  .byte $0F, $31, $35, $39  ; Palette 0 
-  .byte $0F, $16, $1A, $1E  ; Palette 1 
-  .byte $0F, $31, $16, $1A  ; Palette 2
-  .byte $0F, $28, $11, $25  ; Palette 3
+sprites:
+
+; tank face up
+.byte $00, $02, $00, $10
+.byte $00, $03, $00, $18
+.byte $08, $12, $00, $10
+.byte $08, $13, $00, $18
+
+; tank moving up
+.byte $00, $04, $00, $20
+.byte $00, $05, $00, $28
+.byte $08, $14, $00, $20
+.byte $08, $15, $00, $28
+
+; tank looking down
+.byte $00, $06, $00, $30
+.byte $00, $07, $00, $38
+.byte $08, $16, $00, $30
+.byte $08, $17, $00, $38
+
+; tank moving down
+.byte $00, $08, $00, $40
+.byte $00, $09, $00, $48
+.byte $08, $18, $00, $40
+.byte $08, $19, $00, $48
+
 
 ; Character memory
 .segment "CHARS"
